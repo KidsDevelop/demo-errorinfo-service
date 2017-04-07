1b80395e8dec21fe2215ea6ff1f78c4d433e0fdc
Kid Develop <kawee.l@g-able.com>
2017-04-07 14:47:30 +0700

    create errorInfo

---
+++ ErrorInfoService/src/main/java/com/kids/micro/ErrorInfoServiceApplication.java
@@ -0,0 +1,12 @@
+package com.kids.micro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrorInfoServiceApplication.class, args);
+	}
+}
